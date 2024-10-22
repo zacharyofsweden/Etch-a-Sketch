b6de50839c7ddfb3c2b603c3622eaf98f1abab84
zacharyofsweden <zacharyofsweden@gmail.com>
2024-10-22 18:02:45 +0100

    Refectored code into smaller sections

--- myScript.js
+++ myScript.js
@@ -1,17 +1,14 @@
+//GlovalVariabals
 let clears = 0;
-
-//Dom 
-//TODO Refactor code so it all have seprete fucntions for readability. 
-//May have to convert from string to int
-
-//Ui
 let promtText;
+
+//Creating Dom
 const body = document.querySelector("body")
 
-const buttontest = document.createElement("button")
-buttontest.className = "buttonTest"
-buttontest.textContent = "Click Here to chose your grid size!"
-body.appendChild(buttontest)
+const UserGridInputButton = document.createElement("button")
+UserGridInputButton.className = "buttonTest"
+UserGridInputButton.textContent = "Click Here to chose your grid size!"
+body.appendChild(UserGridInputButton)
 
 //Text
 
@@ -20,59 +17,41 @@ textName.textContent = " Etch A SKETCH"
 body.appendChild(textName)
 
 
-
 //grid System
 const containerDiv = document.createElement("div")
 containerDiv.className = "container"
 body.appendChild(containerDiv)
 
 
-//TODO Make sure you can only pick size one (Later on change so it updates every time)
 //TODO Add limit of 100. 
-buttontest.addEventListener("click", () => {
-
-
+UserGridInputButton.addEventListener("click", () => {
     //Promt the user for size of grid
     promtText = prompt("Type a number (Maximum 100)")
+    if (promtText < 100) {
+        console.log("Within range")
 
-    //remove all things in container 
-    if (clears >= 1) {
-        clearScreen()
     }
+    else {
+        promtText = 100;
+    }
+    console.log(promtText)
+    //remove all things in container 
+    clearScreen()
 
-    clears = clears + 1;
-    console.log(clears)
 
     gridCreater()
 
-  
+    userInput()
 
-    //Add event listernes to all block
-    //Make it so you can add color
-    const blocks = document.querySelectorAll(".block");
-    blocks.forEach((block) => {
-        block.addEventListener("click", function (e) {
-            block.setAttribute("style", "background: red;")
-            console.log("Working!")
-        })
-    })
-
-    //TODO make it so you can left click to remove 
+    clears++;
 })
 
-
-function clearScreen() {
-    const grids = document.querySelectorAll(".rowContainer")
-    grids.forEach((grid) => {
-        containerDiv.removeChild(grid)
-    })
-
-}
+//Functions
 
 
 
-function gridCreater() 
-{
+
+function gridCreater() {
     //Creates the grid
     for (let i = 1; i <= promtText; i++) {
         const divRowContainer = document.createElement("div")
@@ -92,8 +71,29 @@ function gridCreater()
 
 
 
-function userInput()
-{
+function userInput() {
+    //Add event listernes to all block
+    //Make it so you can add color
+    const blocks = document.querySelectorAll(".block");
+    blocks.forEach((block) => {
+        block.addEventListener("click", function (e) {
+            block.setAttribute("style", "background: red;")
+            console.log("Working!")
+        })
+    })
+}
+
+//TODO make it so you can left click to remove 
+
+
+function clearScreen() {
+    const grids = document.querySelectorAll(".rowContainer")
+    if (clears >= 1) {
+        grids.forEach((grid) => {
+            containerDiv.removeChild(grid)
+        })
+    }
+
 
 }
 
