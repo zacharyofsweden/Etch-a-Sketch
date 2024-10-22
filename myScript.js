02689c751fece85919b8eebba2477210a6cc2f35
zacharyofsweden <zacharyofsweden@gmail.com>
2024-10-22 12:01:19 +0100

    Fixing 16 x 16 grid

--- myScript.js
+++ myScript.js
@@ -1,16 +1,27 @@
 //Dom 
 
+//figure out how to acces all the children of container in css
 
 
 const containerDiv = document.querySelector(".Container")
 //this is the first row then we will do this 16 times on the other axis using flex box verical and horizantal 
 //Creates 16 divs and adds them to the container
-for (let i = 1; i <= 16; i++) 
+//create a container for 16 then append tp main container and do that 16 times
+//for loop for making container 
+//for (let i = 1; i <= 16; i++) 
 {
-    const div = document.createElement("div")
-    containerDiv.appendChild(div)
-    div.className = "block " + i;
-    console.log("Added " + i)
+    const divRowContainer = document.createElement("div")
+    divRowContainer.className = "Container " + i;
+
+    for (let i = 1; i <= 16; i++) {
+        const div = document.createElement("div")
+        containerDiv.appendChild(divRowContainer)
+        div.className = "block " + i;
+        console.log("Added " + i)
+    }
+
+    containerDiv.appendChild(divRowContainer)
 }
+
 
 //Appending 
