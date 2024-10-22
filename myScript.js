4cad751d3e8e5d9de8f4ea90d9446da2ba722280
zacharyofsweden <zacharyofsweden@gmail.com>
2024-10-22 17:48:25 +0100

    Can now reset grid

--- myScript.js
+++ myScript.js
@@ -1,5 +1,6 @@
+let clears = 0;
+
 //Dom 
-//TODO: add text Minos Etch a sketch! 
 //TODO Refactor code so it all have seprete fucntions for readability. 
 //May have to convert from string to int
 
@@ -9,36 +10,42 @@ const body = document.querySelector("body")
 
 const buttontest = document.createElement("button")
 buttontest.className = "buttonTest"
-buttontest.textContent = "Test"
+buttontest.textContent = "Click Here to chose your grid size!"
 body.appendChild(buttontest)
 
 //Text
 
+const textName = document.createElement("h1")
+textName.textContent = " Etch A SKETCH"
+body.appendChild(textName)
+
 
 
 //grid System
-const containerDiv = document.querySelector(".Container")
+const containerDiv = document.createElement("div")
+containerDiv.className = "container"
+body.appendChild(containerDiv)
+
+
 //TODO Make sure you can only pick size one (Later on change so it updates every time)
 //TODO Add limit of 100. 
 buttontest.addEventListener("click", () => {
+
+
     //Promt the user for size of grid
-    promtText = prompt("Working!")
+    promtText = prompt("Type a number (Maximum 100)")
 
-    //For loop that creates the grid
-    for (let i = 1; i <= promtText; i++) {
-        const divRowContainer = document.createElement("div")
-        divRowContainer.className = "rowContainer " + i;
-
-        for (let i = 1; i <= promtText; i++) {
-            const div = document.createElement("div")
-            divRowContainer.appendChild(div)
-            div.className = "block " + i;
-            console.log("Added " + i)
-        }
-        console.log("Added container" + i)
-        containerDiv.appendChild(divRowContainer)
+    //remove all things in container 
+    if (clears >= 1) {
+        clearScreen()
     }
-    //Event Listiners
+
+    clears = clears + 1;
+    console.log(clears)
+
+    gridCreater()
+
+  
 
     //Add event listernes to all block
     //Make it so you can add color
@@ -54,13 +61,41 @@ buttontest.addEventListener("click", () => {
 })
 
 
+function clearScreen() {
+    const grids = document.querySelectorAll(".rowContainer")
+    grids.forEach((grid) => {
+        containerDiv.removeChild(grid)
+    })
+
+}
+
+
+
+function gridCreater() 
+{
+    //Creates the grid
+    for (let i = 1; i <= promtText; i++) {
+        const divRowContainer = document.createElement("div")
+        divRowContainer.className = "rowContainer " + i;
+
+        for (let i = 1; i <= promtText; i++) {
+            const div = document.createElement("div")
+            divRowContainer.appendChild(div)
+            div.className = "block " + i;
+            console.log("Added " + i)
+        }
+        console.log("Added container" + i)
+        containerDiv.appendChild(divRowContainer)
+    }
+}
 
 
 
 
+function userInput()
+{
 
-
-
+}
 
 
 
