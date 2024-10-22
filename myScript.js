6c47527948192bec1ad1c5daccd761edf332607a
zacharyofsweden <zacharyofsweden@gmail.com>
2024-10-22 12:47:25 +0100

    Click to add color to grid

--- myScript.js
+++ myScript.js
@@ -1,12 +1,10 @@
 //Dom 
-
-//figure out how to acces all the children of container in css
-
+//TODO: Add input so user can chose size of grid.
+//TODO: Make it so when user click it colors the square. (Permenantly)
+//TODO: add text Minos Etch a sketch! 
 
 const containerDiv = document.querySelector(".Container")
-//this is the first row then we will do this 16 times on the other axis using flex box verical and horizantal 
-//Creates 16 divs and adds them to the container
-//create a container for 16 then append tp main container and do that 16 times
+
 //for loop for making container 
 for (let i = 1; i <= 16; i++) {
     const divRowContainer = document.createElement("div")
@@ -18,9 +16,21 @@ for (let i = 1; i <= 16; i++) {
         div.className = "block " + i;
         console.log("Added " + i)
     }
-    console.log("Added container"+ i)
+    console.log("Added container" + i)
     containerDiv.appendChild(divRowContainer)
 }
 
 
-//Appending 
+//Event Listiners 
+
+//Add event listernes to all block 
+//Make it so you can add color 
+const blocks = document.querySelectorAll(".block");
+
+blocks.forEach((block) => {
+    block.addEventListener("click", function (e) {
+        block.setAttribute("style", "background: red;")
+    })
+})
+
+//TODO make it so you can left click to remove 
