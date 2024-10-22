6814d4e508ec4ffa849d43357a62a7fae6902002
zacharyofsweden <zacharyofsweden@gmail.com>
2024-10-22 13:20:56 +0100

    User can control size of grid

--- myScript.js
+++ myScript.js
@@ -1,36 +1,68 @@
 //Dom 
-//TODO: Add input so user can chose size of grid.
-//TODO: Make it so when user click it colors the square. (Permenantly)
 //TODO: add text Minos Etch a sketch! 
+//TODO Refactor code so it all have seprete fucntions for readability. 
+//May have to convert from string to int
 
+//Ui
+let promtText;
+const body = document.querySelector("body")
+
+const buttontest = document.createElement("button")
+buttontest.className = "buttonTest"
+buttontest.textContent = "Test"
+body.appendChild(buttontest)
+
+//Text
+
+
+
+//grid System
 const containerDiv = document.querySelector(".Container")
+//TODO Make sure you can only pick size one (Later on change so it updates every time)
+//TODO Add limit of 100. 
+buttontest.addEventListener("click", () => {
+    //Promt the user for size of grid
+    promtText = prompt("Working!")
 
-//for loop for making container 
-for (let i = 1; i <= 16; i++) {
-    const divRowContainer = document.createElement("div")
-    divRowContainer.className = "rowContainer " + i;
+    //For loop that creates the grid
+    for (let i = 1; i <= promtText; i++) {
+        const divRowContainer = document.createElement("div")
+        divRowContainer.className = "rowContainer " + i;
 
-    for (let i = 1; i <= 16; i++) {
-        const div = document.createElement("div")
-        divRowContainer.appendChild(div)
-        div.className = "block " + i;
-        console.log("Added " + i)
+        for (let i = 1; i <= promtText; i++) {
+            const div = document.createElement("div")
+            divRowContainer.appendChild(div)
+            div.className = "block " + i;
+            console.log("Added " + i)
+        }
+        console.log("Added container" + i)
+        containerDiv.appendChild(divRowContainer)
     }
-    console.log("Added container" + i)
-    containerDiv.appendChild(divRowContainer)
-}
+    //Event Listiners
 
-
-//Event Listiners 
-
-//Add event listernes to all block 
-//Make it so you can add color 
-const blocks = document.querySelectorAll(".block");
-
-blocks.forEach((block) => {
-    block.addEventListener("click", function (e) {
-        block.setAttribute("style", "background: red;")
+    //Add event listernes to all block
+    //Make it so you can add color
+    const blocks = document.querySelectorAll(".block");
+    blocks.forEach((block) => {
+        block.addEventListener("click", function (e) {
+            block.setAttribute("style", "background: red;")
+            console.log("Working!")
+        })
     })
+
+    //TODO make it so you can left click to remove 
 })
 
-//TODO make it so you can left click to remove 
+
+
+
+
+
+
+
+
+
+
+
+
+
